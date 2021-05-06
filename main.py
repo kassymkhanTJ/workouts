20565a337519a9dc0443b5902191f2e96a1679e4
kasimtj <kasimtj@gmail.com>
2021-05-07 01:46:00 +0300

    many changes

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from workouts.paths import workouts_list
 from activities.paths import activities_list
